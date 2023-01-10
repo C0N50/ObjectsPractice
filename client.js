ea6d793524339de17f811e9713e077891acb7200
Connor Ferris <ferr0373@gmail.com>
2023-01-09 18:08:00 -0600

    added function isRed()

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
-console.log('js');
+let card0 = { 
+    suit: 'Hearts',
+    number: 2
+} //end card0
+
+console.log(card0.suit);
+console.log(card0.number);
+
+function isRed (card) {
+    if (card0.suit === 'Hearts' || card0.suit === 'Diamonds') {
+        return true;
+        }
+    else {
+        return false;
+    }
+} //end isRed
+
+console.log('Test isRed', isRed(card0));
